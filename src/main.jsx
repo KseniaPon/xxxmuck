250deeb889731d0d967c6c92d8ac8ac3f107a005
Ksenia Ponomareva <ponomarevaks@gmail.com>
2024-04-21 19:30:01 +0200

    Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { ProductPage } from './components/ProductPage/index.jsx'
+import { ProductPicker } from './components/ProductPicker/index.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    
+    children: [
+      {
+        path: '/',
+        element: <ProductPicker />,
+      },
+      {
+        path: '/product',
+        element: < ProductPage/>,
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
